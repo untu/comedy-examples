8d58f62c72bfd2dfe484c760f81bfd210f188203
weekens <weekens@gmail.com>
2016-11-06 09:02:46 +0300

    Edited comments.

--- forked-actor.js
+++ forked-actor.js
@@ -7,18 +7,20 @@ var actors = require('comedy');
  */
 class MyActor {
   sayHello(to) {
+    // Reply with a message, containing self PID.
     return `Hello ${to} from ${process.pid}!`;
   }
 }
 
+// Create an actor system.
 var actorSystem = actors();
 
 actorSystem
   // Get a root actor reference.
   .rootActor()
-  // Create a class-defined child actor, that is run in a separate process.
+  // Create a class-defined child actor, that is run in a separate process (forked mode).
   .then(rootActor => rootActor.createChild(MyActor, { mode: 'forked' }))
-  // Send a message to out forked actor.
+  // Send a message to our forked actor with a self process PID.
   .then(myActor => myActor.sendAndReceive('sayHello', process.pid))
   .then(reply => {
     console.log(`Actor replied: ${reply}`);
