ff7c1a28ab49abe36969600efa5c3b1aaff7c736
weekens <weekens@gmail.com>
2017-01-24 11:52:12 +0300

    Added comments to MongoDB resource example.

--- mongo-db-resource.js
+++ mongo-db-resource.js
@@ -3,7 +3,16 @@
 var actors = require('comedy');
 var mongodb = require('mongodb');
 
+/**
+ * MongoDB connection resource definition.
+ */
 class MongoDbConnectionResource {
+  /**
+   * Resource initialization logic.
+   *
+   * @param system Actor system instance.
+   * @returns {Promise} Initialization promise.
+   */
   initialize(system) {
     this.log = system.getLog();
     this.log.info('Initializing MongoDB connection resource...');
@@ -16,6 +25,11 @@ class MongoDbConnectionResource {
       })
   }
 
+  /**
+   * Resource destruction logic.
+   *
+   * @returns {Promise} Destruction promise.
+   */
   destroy() {
     this.log.info('Destroying MongoDB connection resource...');
 
@@ -24,34 +38,69 @@ class MongoDbConnectionResource {
     });
   }
 
+  /**
+   * This method returns the actual resource, that will be used by actors.
+   *
+   * @returns {*} MongoDB Database instance.
+   */
   getResource() {
     return this.connection;
   }
 }
 
+/**
+ * Test actor, that works with MongoDB connection resource.
+ */
 class TestActor {
+  /**
+   * @returns {[String]} Names of injected resources (taken from resource class name
+   * or getName() method, if present).
+   */
   static inject() {
     return ['MongoDbConnectionResource'];
   }
 
+  /**
+   * @param mongoDb MongoDB Database instance (injected by Comedy).
+   */
   constructor(mongoDb) {
     this.mongoDb = mongoDb;
   }
 
+  /**
+   * Actor initialization logic.
+   *
+   * @param selfActor Self actor instance.
+   */
   initialize(selfActor) {
-    return this.mongoDb.collection('test').find({}).toArray().then(result => {
+    this.log = selfActor.getLog();
+  }
+
+  /**
+   * Dumps a given collection to stdout.
+   *
+   * @param {String} name Collection name.
+   * @returns {Promise} Operation promise.
+   */
+  dumpCollection(name) {
+    return this.mongoDb.collection(name).find({}).toArray().then(result => {
       result.forEach((obj, idx) => {
-        selfActor.getLog().info(`Query result #${idx}: ${JSON.stringify(obj, null, 2)}`);
+        this.log.info(`Collection "${name}", item #${idx}: ${JSON.stringify(obj, null, 2)}`);
       });
     });
   }
 }
 
+// Create actor system with MongoDB connection resource defined.
 var system = actors({
   resources: [MongoDbConnectionResource]
 });
 
 system
   .rootActor()
+  // Create test actor.
   .then(rootActor => rootActor.createChild(TestActor))
+  // Send a 'dumpCollection' message and wait for processing to finish.
+  .then(testActor => testActor.sendAndReceive('dumpCollection', 'test'))
+  // Destroy the system.
   .finally(() => system.destroy());
