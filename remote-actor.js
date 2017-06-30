55ba4c16174a6beee7e32a790a2ebdea80a94d6c
weekens <weekens@gmail.com>
2017-06-30 12:17:55 +0300

    Adjusted remote actor example.

--- remote-actor.js
+++ remote-actor.js
@@ -1,6 +1,28 @@
 'use strict';
 
+/**
+ * To successfully run this example, you need to first launch a Comedy listener
+ * node on this host. To do that, run:
+ *
+ * $ node_modules/.bin/comedy-node
+ *
+ * You can also launch a listener node on the host of your choice and change the
+ * remote IP address in this example (remoteIpAddress variable) to check real
+ * network communication.
+ *
+ * To start listener node on remote machine, do:
+ *
+ * $ npm install comedy
+ * $ node_modules/.bin/comedy-node
+ *
+ * in any folder on remote machine.
+ */
+
 var actors = require('comedy');
+
+// Remote IP address. You can change it to a different IP address.
+// If Comedy node is started and accessible on that address, the example will work.
+var remoteIpAddress = '127.0.0.1';
 
 /**
  * Actor definition class.
@@ -19,7 +41,7 @@ actorSystem
 // Get a root actor reference.
   .rootActor()
   // Create a class-defined child actor, that is run on a remote machine (remote mode).
-  .then(rootActor => rootActor.createChild(MyActor, { mode: 'remote', host: '192.168.33.20' }))
+  .then(rootActor => rootActor.createChild(MyActor, { mode: 'remote', host: remoteIpAddress }))
   // Send a message to our remote actor with a self process PID.
   .then(myActor => myActor.sendAndReceive('sayHello', process.pid))
   .then(reply => {
